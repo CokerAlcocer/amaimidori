f833b4dc7a540c53e6e385fd6901ee6dfc7a074a
CokerAlcocer <angelalcocer@utez.edu.mx>
2025-06-02 15:03:28 -0600

    [UPDATE] Se finalizaron los apartados de la página

---
+++ src/js/scrollspy.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-link");
+    
+    const observer = new IntersectionObserver(
+        entries => {
+            entries.forEach(entry => {
+                const id = entry.target.getAttribute("id");
+                const navLink = document.querySelector(`.nav-link[href="#${id}"]`);
+                
+                if (entry.isIntersecting) {
+                    navLinks.forEach(link => link.classList.remove("active"));
+                    navLink.classList.add("active");
+                }
+            });
+        },
+        {
+            threshold: 0.6, // 60% visible
+        }
+    );
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
